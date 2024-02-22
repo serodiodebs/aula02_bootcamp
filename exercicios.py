dcba43c10bbb98fa953576c22619bb6107a5e308
serodiodebs <32377390+serodiodebs@users.noreply.github.com>
2024-02-21 22:11:57 -0300

    adicionado exercicio 4

--- exercicios.py
+++ exercicios.py
@@ -11,11 +11,15 @@
 # print(f"O resto de {numero1} dividido por 5 é: {resto}")
 
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
-n1 = int(input("Digite o 1º número: "))
-n2 = int(input("Digite o 2º número: "))
-print(n1 * n2)
+# n1 = int(input("Digite o 1º número: "))
+# n2 = int(input("Digite o 2º número: "))
+# print(n1 * n2)
 
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+n1 = int(input("Digite o 1º número: "))
+n2 = int(input("Digite o 2º número: "))
+print(n1 // n2)
+
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
 
 # #### Números de Ponto Flutuante (`float`)
