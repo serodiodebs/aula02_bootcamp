27b045fd941a97f8750f10be2bc1758405c9e83a
serodiodebs <32377390+serodiodebs@users.noreply.github.com>
2024-02-23 20:17:00 -0300

    adicionado exercicio 15

--- exercicios.py
+++ exercicios.py
@@ -65,13 +65,17 @@
 # print(f"A frase é:{frase_ok}")
 
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
-data = input("Digite uma data no formato 'dd/mm/aaa': ")
-data_splitada = data.split("/")
-print(f"Dia: {data_splitada[0]}")
-print(f"Mês: {data_splitada[1]}")
-print(f"Ano: {data_splitada[2]}")
+# data = input("Digite uma data no formato 'dd/mm/aaa': ")
+# data_splitada = data.split("/")
+# print(f"Dia: {data_splitada[0]}")
+# print(f"Mês: {data_splitada[1]}")
+# print(f"Ano: {data_splitada[2]}")
 
 # 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
+palavra1 = input("Digite uma palavra: ")
+palavra2 = input("Digite mais uma palavra: ")
+
+print(f"Você digitou as palavras: {palavra1+palavra2}")
 
 # #### Booleanos (`bool`)
 
